d3af3317c98aa43e4a7f72db9c65b1b5036903e3
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-28 22:09:57 +0530

    medis

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
-
+// frontend/src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Home from './components/Home';
+import Logout from './components/Logout';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
@@ -11,7 +13,8 @@ function App() {
       <Switch>
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        <Route path="/" component={Home} />
+        <Route path="/logout" component={Logout} />
+        <ProtectedRoute path="/" component={Home} /> {/* Protect Home Route */}
       </Switch>
     </Router>
   );
